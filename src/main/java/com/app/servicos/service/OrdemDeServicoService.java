027e13b6c00041e1a0ddaa403ea70b3f0dce8c69
carloslaczynskizup <carlos.laczynski@zup.com.br>
2024-05-04 12:00:13 -0300

    imprefatorando as eresponse http da listagem de ordens

--- src/main/java/com/app/servicos/service/OrdemDeServicoService.java
+++ src/main/java/com/app/servicos/service/OrdemDeServicoService.java
@@ -2,7 +2,6 @@ package com.app.servicos.service;
 
 import com.app.servicos.entity.ClientePF;
 import com.app.servicos.entity.ClientePJ;
-import com.app.servicos.entity.Endereco;
 import com.app.servicos.entity.OrdemDeServico;
 import com.app.servicos.enums.TipoCliente;
 import com.app.servicos.repository.ClientePFRepository;
@@ -59,24 +58,13 @@ public class OrdemDeServicoService {
         return ordemDeServicoRepository.save(ordemDeServico);
     }
 
-    public List<OrdemDeServico> listarOrdensDeServico() {
-
+    public Optional<List<OrdemDeServico>> listarOrdensDeServico() {
         List<OrdemDeServico> ordens = ordemDeServicoRepository.findAll();
-
-        if (ordens.isEmpty()) {
-            throw new RuntimeException("Nenhuma ordem de serviço encontrada.");
-        }
-        return ordens;
+        return ordens.isEmpty() ? Optional.empty() : Optional.of(ordens);
     }
 
     public Optional<OrdemDeServico> buscarOrdemDeServicoPorId(Long id) {
-
-        Optional<OrdemDeServico> ordem = ordemDeServicoRepository.findById(id);
-
-        if (ordem.isEmpty()) {
-            throw new RuntimeException("Ordem de serviço não encontrada para o ID: " + id);
-        }
-        return ordem;
+        return ordemDeServicoRepository.findById(id);
     }
 
     public Optional<OrdemDeServico> atualizarOrdemDeServico(Long ordemDeServicoId, OrdemDeServico ordemDeServicoAtualizada) {
